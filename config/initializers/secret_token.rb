f6e517300437b824ae469263939c1544398177f2
kami <skymay1@gmail.com>
2014-02-02 11:54:07 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Amaterasu::Application.config.secret_key_base = 'da975fdcbb0325e104f11a1bc4c822b4cd456618ee5f552c015674bb30b7623b03597d0b54bb2f67609cc168f4f8b2c59b9188840c3f64d70abb7db2aeb03405'
